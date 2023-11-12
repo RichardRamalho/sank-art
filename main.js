368f15fb4f3d8f1ecdab1ebc154225d9b0803b29
RichardRamalho <128335563+RichardRamalho@users.noreply.github.com>
2023-11-12 12:49:48 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function WhatsAppOpen() {
+    var number = '+5511939446812';
+
+    window.open ('https://api.whatsapp.com/send/?phone=%2B5511939446812&text&type=phone_number&app_absent=0', '_blank');
+}
+
+  
